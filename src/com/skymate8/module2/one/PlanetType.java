aabaf0bfe5ca8033a11c9cc8f1e0ced9e23b5da4
cloudate8 <144731911+eellvviinn@users.noreply.github.com>
2023-11-09 13:22:53 -0800

    Module2.1
    qwerty

---
+++ src/com/skymate8/module2/one/PlanetType.java
@@ -0,0 +1,7 @@
+package com.skymate8.module2.one;
+
+public enum PlanetType {
+
+    BIG, DWARF, GREEN, NUCLEAR, DEFORMED, SQUARE;
+
+}
---
+++ src/com/skymate8/module2/two/DickOfCards.java
@@ -0,0 +1,7 @@
+package com.skymate8.module2.two;
+
+public class DeckOfCards {
+
+    private CardSuits suit;
+    private 
+}
